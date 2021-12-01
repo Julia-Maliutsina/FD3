022b4fbc81707430647f60fb61caf3560f638002
Julia <julia.maliutsina@gmail.com>
2021-12-01 21:16:46 +0300

    task RC299

---
+++ ishop2/IShop.js
@@ -0,0 +1,53 @@
+const IShopComponent = React.createClass({
+
+  displayName: 'IShopComponent',
+
+  getInitialState: function () {
+    return {
+      products: this.props.goods,
+      selectedKey: NaN,
+      productToDelete: NaN
+    }
+  },
+
+  selectProduct: function (key) {
+    this.setState({selectedKey: key});
+  },
+
+  deleteProduct: function (key, EO) {
+    EO.stopPropagation();
+    let confirmDelete = confirm("Delete product from list?");
+    if (confirmDelete) {
+      let productsArray = [...this.state.products];
+      productsArray = productsArray.filter(product => product.key!==key);
+      this.setState({products: productsArray})
+    }
+  },
+
+  render: function() {
+
+    let headTable=React.DOM.thead({className: 'tableHead'}, 
+      React.DOM.tr({className:'tableHeadRow'}, 
+        React.DOM.th({className: 'ProductImgInTable'},"Photo"),
+        React.DOM.th({className: 'ProductNameInTable'},"Product"),
+        React.DOM.th({className: 'ProductPriceInTable'},"Price"),
+        React.DOM.th({className: 'ProductLeftInTable'},"Left in stock"),
+      )
+    );
+
+    const bodyTable=this.state.products.map( product =>
+      React.createElement(Product, 
+        {product: product, selected: this.state.selectedKey,
+        changeSelected: this.selectProduct, deleteProduct: this.deleteProduct
+      })
+    );
+    
+    let offersTableBody = React.DOM.tbody({className: 'tableBody'}, bodyTable);
+
+    return React.DOM.div( {className:'IShopComponent'}, 
+      React.DOM.div( {className:'ShopName'}, this.props.shopName ),
+      React.DOM.table( {className:'OffersTable'}, headTable, offersTableBody  ),
+    );
+  },
+
+});
---
+++ ishop2/Product.js
@@ -0,0 +1,35 @@
+const Product = React.createClass({
+
+  displayName: 'Product',
+
+  propTypes: {
+    product: React.PropTypes.object.isRequired,
+    selected: React.PropTypes.number.isRequired,
+    changeSelected: React.PropTypes.func.isRequired,
+    deleteProduct: React.PropTypes.func.isRequired,
+  },
+
+  rowClicked: function(EO) {
+    this.props.changeSelected(this.props.code);
+  },
+
+  render: function() {
+
+    const product = this.props.product;
+    let classProduct = 'Product';
+    if (product.key===this.props.selected) {
+      classProduct='ProductSelected'; 
+    }
+
+    return React.DOM.tr(
+      {key: product.key, className: classProduct, onClick: ()=>this.props.changeSelected(product.key)},
+      React.DOM.td({className: 'ProductImgInTable'}, 
+        React.DOM.img ({className: 'ProductImg', src:product.photo, alt:product.name})
+      ),
+      React.DOM.td({className: 'ProductNameInTable'},product.name),
+      React.DOM.td({className: 'ProductPriceInTable'},product.price + "$"),
+      React.DOM.td({className: 'ProductLeftInTable'},product.left),
+      React.DOM.td({className: 'ProductDeleteInTable', onClick: (EO)=>this.props.deleteProduct(product.key, EO)},'delete'),
+    );
+  }
+})
